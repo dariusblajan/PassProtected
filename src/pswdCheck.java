205218ef21dee297059f18eb736953ab99ec13c0
dariusblajan <dariusblajan@gmail.com>
2015-02-27 21:21:48 +0200

    searches name and pass in userslist.txt

--- src/pswdCheck.java
+++ src/pswdCheck.java
@@ -1,32 +1,47 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 
 public class pswdCheck {
+    public static boolean isOnTheList(File f, String s) throws FileNotFoundException {
+        Scanner scanner = new Scanner(f);       // procedura care cauta linia s in fisierul f
+        String line =scanner.nextLine();  //read first line
+        boolean found =false;
+        while (!line.isEmpty()){        //while line is not empty
+            if (line.equals(s)) {       //if line s is a match with the line previously read
+                found=true;
+                break;                  //stop searching
+            }
+            line =scanner.nextLine();   //reading next line in file f
+        }
+        return found;
+    }
     public static void main(String[] args) throws IOException {
         String user;
         String password;
         Scanner read = new Scanner(System.in);
         File file = new File("userslist.txt");
         Scanner scanner = new Scanner(file);
-        String line =scanner.nextLine();
-        //System.out.println(line);
-        String[] parsed=line.split(",");
-        boolean isCorrect=false;
         Runtime run = Runtime.getRuntime();
+        boolean isCorrect = false;
+
+        /*while (!line.isEmpty()) {
+            String[] parsed = line.split(",");
+
+        }
+            line =scanner.nextLine();
+        }*/
         do{
             System.out.print("user: ");
             user = read.nextLine();
             System.out.print("pass: ");
             password = read.nextLine();
-            if (parsed[0].equals(user) && parsed[1].equals(password)) {
+            String aux=user+","+password; //creating a specific line format, spit by ",": user,password to be
+            if (isOnTheList(file,aux)) {  //verified in file
                 isCorrect=true;
                 run.exec("notepad");
             }
-        } while (!isCorrect);
         System.out.println("success.");
-    }
+    }while (!isCorrect);
+}
 }
